46e7256e24b716363a7084b631c99e0fdbb5ac2d
Sooraj B <sooraj.b@webandcrafts.com>
2023-11-26 00:09:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors({ origin: "*" }));
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => console.log(`server running on Port:${PORT}`));
