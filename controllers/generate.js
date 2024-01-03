6fe5bc2455fbe02e6e8579c2662289cddc8c113f
Sooraj B <sooraj.b@webandcrafts.com>
2024-01-03 23:52:47 +0530

    final content console added

--- controllers/generate.js
+++ controllers/generate.js
@@ -6,25 +6,8 @@ const openai = new OpenAI({
   apiKey: process.env.OPEN_AI_TOKEN,
 });
 
-const getRespose = async (content) => {
-  const completion = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "system",
-        content:
-          "you generate blogs, the responses should be in html with the content inside appropriate html tags",
-      },
-      { role: "user", content },
-    ],
-    model: "gpt-3.5-turbo",
-  });
-
-  return completion.choices[0];
-};
-
 export const generateBlog = async (req, res) => {
   const { content } = req.body;
-  console.log(content, ":content");
   if (content) {
     try {
       const stream = await openai.chat.completions.create({
@@ -33,7 +16,7 @@ export const generateBlog = async (req, res) => {
           {
             role: "system",
             content:
-              "you generate blogs, the responses should be in html with the content inside appropriate html tags",
+              "You only give response in html. You are generating blogs. Every response should be in html.",
           },
           { role: "user", content },
         ],
@@ -46,9 +29,14 @@ export const generateBlog = async (req, res) => {
         Connection: "keep-alive",
       });
 
+      let finalStreamContent = "";
+
       for await (const part of stream) {
-        res.write(`${part.choices[0]?.delta?.content}`);
+        res.write(part.choices[0]?.delta?.content ?? "");
+        finalStreamContent += part.choices[0]?.delta?.content ?? "";
       }
+
+      console.log(finalStreamContent);
 
       res.end();
     } catch (error) {
--- index.js
+++ index.js
@@ -16,9 +16,7 @@ app.use("/api/openai", generateRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.DATABASE_URL, {
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.DATABASE_URL)
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
